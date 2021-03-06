0d6caf068c56d7de7f31e85df730c68eefbb8b4f
Waleed <waleed.gatosorriso@gmail.com>
2021-03-06 18:29:20 +0100

    Configuring Tailwind

--- social/routes/web.php
+++ social/routes/web.php
@@ -2,17 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route for home page
+Route::get('/', function(){
+    return view('layouts.home');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+// Route for posts
+Route::get('/posts', function () {
+    return view('posts.index');
 });
---
+++ social/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: ["./resources/**/*.blade.php", "./resources/**/*.js"], // These are used for optimization in production build
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
